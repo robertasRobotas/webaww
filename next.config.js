cdae1441e410f5ecad5ebcb5af3a420980ea9f41
Robertas <r.ankudovicius@gmail.com>
2023-06-01 15:07:12 +0300

    Finished emailing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    EMAILJS_SERVICE_ID: "service_hh7tyh7",
+    EMAILJS_TEMPLATE_ID: "template_4jdhusy",
+    EMAILJS_PUBLIC_KEY: "zIh6O7RJ1LYsBXGxs",
+  },
+};
 
 module.exports = nextConfig
--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Nav } from "../../components/navbar";
+import throttle from "lodash/throttle";
 import i18next from "i18next";
 import { initReactI18next, useTranslation } from "react-i18next";
-import services from "../../assets/services.jpg";
-import webPages from "../../assets/web-pages.jpg";
+import { ToastContainer, toast } from "react-toastify";
 import emailPhoto from "../../assets/email.png";
-import aiSolutions from "../../assets/ai-solutions.jpg";
-import arrow from "../../assets/arrow.png";
 import plane from "../../assets/plane.svg";
+import emailjs from "@emailjs/browser";
+import "react-toastify/dist/ReactToastify.css";
 
 import { Footer } from "@/components/footer";
 import { translationLt } from "../../translations/translationsLt";
@@ -26,13 +25,88 @@ i18next.use(initReactI18next).init({
 
 export default function Home() {
   const { t } = useTranslation();
-  const [name, useName] = useState<string>();
-  const [email, useEmail] = useState<string>();
-  const [company, useCompany] = useState<string>();
-  const [message, useMessage] = useState<string>();
+  const [name, useName] = useState<string>("");
+  const [email, useEmail] = useState<string>("");
+  const [company, useCompany] = useState<string>("");
+  const [message, useMessage] = useState<string>("");
+  const [freeConsultation, setFreeConsultation] = useState<boolean>(false);
+  const [terms, setTerms] = useState<boolean>(false);
+
+  const form = useRef<any>();
+
+  const showSuccessAlert = () =>
+    toast.success("Sent!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+
+  const showErrorAlert = () =>
+    toast.error("Please agree with terms & conditions", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+
+  const showFillDataAlert = () =>
+    toast.error("Please fill all the data", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+
+  const onSubmitMessage = (e: any) => {
+    if (!name || !email || !company || !message) {
+      showFillDataAlert();
+      return;
+    }
+
+    if (terms) {
+      e.preventDefault();
+      emailjs
+        .sendForm(
+          process.env.EMAILJS_SERVICE_ID as string,
+          process.env.EMAILJS_TEMPLATE_ID as string,
+          form.current,
+          process.env.EMAILJS_PUBLIC_KEY as string
+        )
+        .then(
+          (result) => {
+            console.log("Sent");
+            useName("");
+            useEmail("");
+            useCompany("");
+            useMessage("");
+            showSuccessAlert();
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    } else {
+      showErrorAlert();
+    }
+  };
 
   return (
     <>
+      <ToastContainer />
+
       <div className="container mx-auto max-w-screen-xl px-6 md:px-8 md:max-w-[880px] lg:max-w-[1024px] xl:max-w-[1280px] 2xl:max-w-[1440px] pb-8">
         <Nav />
         <div className="grid gap-8 md:grid-cols-2">
@@ -41,43 +115,66 @@ export default function Home() {
               {t("contactsTitle")}
             </h1>
             <h4 className="font-medium">{t("contactsSubtitle")}</h4>
-            <input
-              placeholder={t("yourName") as string}
-              className="py-2 px-4 mt-4 rounded-lg text-sm"
-              value={name}
-              onChange={(e) => useName(e.target.value)}
-            />
-            <input
-              placeholder={t("yourEmail") as string}
-              className="py-2 px-4 rounded-lg text-sm"
-              value={email}
-              onChange={(e) => useEmail(e.target.value)}
-            />
-            <input
-              placeholder={t("companyName") as string}
-              className="py-2 px-4 rounded-lg text-sm"
-              value={company}
-              onChange={(e) => useCompany(e.target.value)}
-            />
-            <textarea
-              placeholder={t("yourMessage") as string}
-              className="py-2 px-4 rounded-lg resize-none h-28 text-sm"
-              value={message}
-              onChange={(e) => useMessage(e.target.value)}
-            />
+            <form ref={form} className="flex flex-col gap-3">
+              <input
+                name="name"
+                placeholder={t("yourName") as string}
+                className="py-2 px-4 mt-4 rounded-lg text-sm"
+                value={name}
+                onChange={(e) => useName(e.target.value)}
+              />
+              <input
+                name="email"
+                placeholder={t("yourEmail") as string}
+                className="py-2 px-4 rounded-lg text-sm"
+                value={email}
+                onChange={(e) => useEmail(e.target.value)}
+              />
+              <input
+                name="company"
+                placeholder={t("companyName") as string}
+                className="py-2 px-4 rounded-lg text-sm"
+                value={company}
+                onChange={(e) => useCompany(e.target.value)}
+              />
+              <textarea
+                name="message"
+                placeholder={t("yourMessage") as string}
+                className="py-2 px-4 rounded-lg resize-none h-28 text-sm"
+                value={message}
+                onChange={(e) => useMessage(e.target.value)}
+              />
 
-            <div className="flex items-center gap-2">
-              <input type="checkbox" className="w-5 h-5" />
-              <span>I would like to get a free consultation.</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <input type="checkbox" className="w-5 h-5" />
-              <span>I agree with your privacy policy.</span>
-            </div>
-            <button className="bg-white w-28 py-1 rounded-lg flex gap-2 items-center justify-center">
-              <span>Send</span>
-              <img src={plane.src} className="h-4 w-4" />
-            </button>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="w-5 h-5"
+                  name="isConsultation"
+                  checked={freeConsultation}
+                  onChange={() =>
+                    setFreeConsultation((prevState) => !prevState)
+                  }
+                />
+                <span>I would like to get a free consultation.</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="w-5 h-5"
+                  checked={terms}
+                  onChange={() => setTerms((prevState) => !prevState)}
+                />
+                <span>I agree with your privacy policy.</span>
+              </div>
+              <button
+                type="button"
+                onClick={throttle(onSubmitMessage, 2000)}
+                className="bg-white w-28 py-1 rounded-lg flex gap-2 items-center justify-center"
+              >
+                <span>Send</span>
+                <img src={plane.src} className="h-4 w-4" />
+              </button>
+            </form>
           </div>
           <div className="flex items-center">
             <Image alt="email" src={emailPhoto} />
