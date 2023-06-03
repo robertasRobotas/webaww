1c27e5ba34411e81dd165440b33afcca1a7d2702
Robertas <r.ankudovicius@gmail.com>
2023-06-03 11:34:18 +0300

    Fixing SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>webAww - AI Solutions</title>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="AI solutions for business" />
+        <meta
+          name="keywords"
+          content="AI, ChatGPT, react components, react, web pages"
+        />
+        <meta name="author" content="webAww" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
